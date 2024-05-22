bb8d9faccbf2e5dbf2d765e6ab802faf9a788b40
Kroese <kroese@users.noreply.github.com>
2024-05-22 15:41:26 +0200

    Update install.sh

--- src/install.sh
+++ src/install.sh
@@ -9,12 +9,12 @@ detect () {
 
   dir=$(isoinfo -f -i "$file")
 
-  # Automaticly detect EFI-compatible ISO's
+  # Automaticly detect UEFI-compatible ISO's
   if echo "${dir^^}" | grep -qx "/BOOTMGR.EFI;1\|/EFI/BOOT/BOOTX64.EFI;1\|/EFI/BOOT/BOOTX86.EFI;1"; then
     [ -z "$BOOT_MODE" ] && BOOT_MODE="uefi"
   fi
 
-  BOOT="$file"  
+  BOOT="$file"
   return 0
 }
 
