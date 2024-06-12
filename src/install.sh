146f1f6916794411213463c3a2272e99ca10dea9
Kroese <kroese@users.noreply.github.com>
2024-06-12 13:30:52 +0200

    Update install.sh

--- src/install.sh
+++ src/install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-detectMode () {
+detectType() {
 
   local dir=""
   local file="$1"
@@ -9,12 +9,26 @@ detectMode () {
   [ ! -f "$file" ] && return 1
   [ ! -s "$file" ] && return 1
 
-  if [ -z "${BOOT_MODE:-}" ]; then
-    # Automaticly detect UEFI-compatible ISO's
-    dir=$(isoinfo -f -i "$file")
-    dir=$(echo "${dir^^}" | grep "^/EFI")
-    [ -n "$dir" ] && BOOT_MODE="uefi"
-  fi
+  case "${file,,}" in
+    *".iso" )
+
+      [ -n "${BOOT_MODE:-}" ] && break
+
+      # Automaticly detect UEFI-compatible ISO's
+      dir=$(isoinfo -f -i "$file")
+
+      [ -z "$dir" ] && error "Failed to read ISO file, invalid format!" && return 1
+
+      dir=$(echo "${dir^^}" | grep "^/EFI")
+      [ -n "$dir" ] && BOOT_MODE="uefi"
+
+      ;;
+    *".img" | *".raw" | *".qcow2" | *".ova")
+
+      ;;
+    * )
+      error "Unknown file format, extension \".${file/*./}\" is not recognized!" && return 1 ;;
+  esac
 
   BOOT="$file"
   return 0
@@ -24,9 +38,9 @@ downloadFile() {
 
   local url="$1"
   local base="$2"
-  local dest="$3"  
-  local rc total progress
+  local msg rc total progress
 
+  local dest="$STORAGE/$base.tmp"
   rm -f "$dest"
 
   # Check if running with interactive TTY or redirected to docker log
@@ -36,8 +50,9 @@ downloadFile() {
     progress="--progress=dot:giga"
   fi
 
-  local msg="Downloading $base"
-  info "$msg..." && html "$msg..."
+  msg="Downloading image"
+  info "Downloading $base..."
+  html "$msg..."
 
   /run/progress.sh "$dest" "0" "$msg ([P])..." &
 
@@ -50,7 +65,9 @@ downloadFile() {
     if [ "$total" -lt 100000 ]; then
       error "Invalid image file: is only $total bytes?" && return 1
     fi
-    html "Download finished successfully..." && return 0
+    html "Download finished successfully..."
+    mv -f "$dest" "$STORAGE/$base"
+    return 0
   fi
 
   msg="Failed to download $url"
@@ -64,29 +81,95 @@ downloadFile() {
 
 file=$(find / -maxdepth 1 -type f -iname boot.iso | head -n 1)
 [ ! -s "$file" ] && file=$(find "$STORAGE" -maxdepth 1 -type f -iname boot.iso | head -n 1)
-detectMode "$file" && return 0
+detectType "$file" && return 0
 
 if [ -z "$BOOT" ] || [[ "$BOOT" == *"example.com/image.iso" ]]; then
   hasDisk && return 0
-  error "No boot disk specified, set BOOT= to the URL of an ISO file." && exit 64
+  error "No boot disk specified, set BOOT= to the URL of an image file." && exit 64
 fi
-
-base=$(basename "$BOOT")
-detectMode "$STORAGE/$base" && return 0
 
 base=$(basename "${BOOT%%\?*}")
 : "${base//+/ }"; printf -v base '%b' "${_//%/\\x}"
 base=$(echo "$base" | sed -e 's/[^A-Za-z0-9._-]/_/g')
-detectMode "$STORAGE/$base" && return 0
 
-TMP="$STORAGE/${base%.*}.tmp"
+case "${base,,}" in
+  *".iso" )
+    detectType "$STORAGE/$base" && return 0 ;;
+  *".img" | *".raw" | *".qcow2" | *".ova" | "*.vdi" | "*.vmdk" )
+    detectType "$STORAGE/$base" && return 0 ;;
+  *".gz" | *".gzip" | *".xz" | *".7z" | *".zip" | *".rar" | *".lzma" | *".bz" | *".bz2" )
+    detectType "$STORAGE/${base%.*}" && return 0 ;;
+  * )
+    error "Unknown file format, extension \".${base/*./}\" is not recognized!" && exit 33 ;;
+esac
 
-if ! downloadFile "$BOOT" "$base" "$TMP"; then
-  rm -f "$TMP"
+if ! downloadFile "$BOOT" "$base"; then
+  rm -f "$STORAGE/$base.tmp"
   exit 60
 fi
 
-mv -f "$TMP" "$STORAGE/$base"
-! detectMode "$STORAGE/$base" && exit 63
+if [[ "${base,,}" == *".iso" ]]; then
+ ! detectType "$STORAGE/$base" && exit 63
+ return 0
+fi
+
+case "${base,,}" in
+  *".gz" | *".gzip" | *".xz" | *".7z" | *".zip" | *".rar" | *".lzma" | *".bz" | *".bz2" )
+    info "Extracting $base..."
+    html "Extracting image..." ;;
+esac
+
+case "${base,,}" in
+  *".gz" | *".gzip" )
+    gzip -dc "$STORAGE/$base" > "$STORAGE/${base%.*}"
+    base="${base%.*}"
+    ;;
+  *".xz" )
+    xz -dc "$STORAGE/$base" > "$STORAGE/${base%.*}"
+    base="${base%.*}"
+    ;;
+  *".7z" | *".zip" | *".rar" | *".lzma" | *".bz" | *".bz2" )
+
+    tmp="$STORAGE/extract"
+    rm -rf "$tmp"
+    mkdir -p "$tmp"
+    7z x "$STORAGE/$base" -o"$tmp" > /dev/null
+
+    rm -f "$STORAGE/$base"
+    base="${base%.*}"
+
+    if [ ! -s "$tmp/$base" ]; then
+      rm -rf "$tmp"
+      error "Cannot find file \"${base}\" in .${BOOT/*./} archive!" && exit 32
+    fi
+
+    mv "$tmp/$base" "$STORAGE/$base"
+    rm -rf "$tmp"
+
+    ;;
+esac
+
+if [[ "${base,,}" == *".iso" ]]; then
+ ! detectType "$STORAGE/$base" && exit 63
+ return 0
+fi
+
+# QCOW2, VDI, VHD, VHDX, VMDK
+
+case "${base,,}" in
+  *".img" | *".raw" | *".ova" | *".vdi" | "*.vmdk" )
+    mv -f "$dest" "$STORAGE/data.img"
+    BOOT=""
+    return 0 ;;
+  *".qcow2" )
+    BOOT_MODE="uefi"
+    mv -f "$STORAGE/$base" "$STORAGE/data.qcow2"
+    BOOT=""
+    return 0 ;;
+  * )
+    error "Unknown file format, extension \".${base/*./}\" is not recognized!" && exit 33 ;;
+esac
+
+! detectType "$STORAGE/$base" && exit 63
 
 return 0
