135db3f9fa1a4a7d898fc84d07487d49472d5e0a
Kroese <kroese@users.noreply.github.com>
2025-03-03 13:30:34 +0100

    Update network.sh

--- src/network.sh
+++ src/network.sh
@@ -59,6 +59,17 @@ configureDHCP() {
       fi ;;
   esac
 
+  if ! ip link set dev "$VM_NET_TAP" mtu "$MTU"; then
+    warn "Failed to set MTU size.."
+  fi
+
+  GATEWAY_MAC=$(echo "$VM_NET_MAC" | rev)
+  GATEWAY_MAC="02:${GATEWAY_MAC:0:14}"
+
+  if ! ip link set dev "$VM_NET_TAP" address "$GATEWAY_MAC"; then
+    warn "Failed to set gateway MAC address.."
+  fi
+
   while ! ip link set "$VM_NET_TAP" up; do
     info "Waiting for MAC address $VM_NET_MAC to become available..."
     sleep 2
@@ -202,7 +213,7 @@ configureNAT() {
 
   # Create a bridge with a static IP for the VM guest
 
-  { ip link add dev dockerbridge mtu "$MTU" type bridge ; rc=$?; } || :
+  { ip link add dev dockerbridge type bridge ; rc=$?; } || :
 
   if (( rc != 0 )); then
     error "Failed to create bridge. $ADD_ERR --cap-add NET_ADMIN" && return 1
