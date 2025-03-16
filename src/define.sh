1382aad521726f7c19803614db1bc8aefc98610d
Kroese <kroese@users.noreply.github.com>
2025-03-16 08:35:25 +0100

    Update define.sh

--- src/define.sh
+++ src/define.sh
@@ -8,19 +8,19 @@ getURL() {
   local name=""
 
   case "${id,,}" in
-    "alma" )
+    "alma" | "almalinux" | "alma-linux" )
       name="AlmaLinux"
       url="https://repo.almalinux.org/almalinux/9/live/x86_64/AlmaLinux-9.5-x86_64-Live-GNOME.iso" ;;
-    "alpine" )
+    "alpine" | "alpinelinux" | "alpine-linux" )
       name="Alpine Linux"
       url="https://dl-cdn.alpinelinux.org/alpine/v3.19/releases/x86_64/alpine-virt-3.19.1-x86_64.iso" ;;
-    "arch" )
+    "arch" | "archlinux" | "arch-linux" )
       name="Arch Linux"
       url="https://geo.mirror.pkgbuild.com/iso/2025.03.01/archlinux-x86_64.iso" ;;
     "cachy" | "cachyos" )
       name="CachyOS"
       url="https://cdn77.cachyos.org/ISO/desktop/250202/cachyos-desktop-linux-250202.iso" ;;
-    "centos" )
+    "centos" | "centosstream" | "centos-stream" )
       name="CentOS Stream"
       url="https://mirrors.xtom.de/centos-stream/10-stream/BaseOS/x86_64/iso/CentOS-Stream-10-latest-x86_64-dvd1.iso" ;;
     "debian" )
@@ -29,13 +29,13 @@ getURL() {
     "endeavour" | "endeavouros" )
       name="EndeavourOS"
       url="https://mirrors.gigenet.com/endeavouros/iso/EndeavourOS_Mercury-2025.02.08.iso" ;;
-    "fedora" )
+    "fedora" | "fedoralinux" | "fedora-linux" )
       name="Fedora Linux"
       url="https://download.fedoraproject.org/pub/fedora/linux/releases/41/Workstation/x86_64/iso/Fedora-Workstation-Live-x86_64-41-1.4.iso" ;;
-    "gentoo" )
+    "gentoo" | "gentoolinux" | "gentoo-linux" )
       name="Gentoo Linux"
       url="https://distfiles.gentoo.org/releases/amd64/autobuilds/20250309T170330Z/livegui-amd64-20250309T170330Z.iso" ;;
-    "kali" )
+    "kali" | "kalilinux" | "kali-linux" )
       name="Kali Linux"
       url="https://cdimage.kali.org/kali-2024.4/kali-linux-2024.4-live-amd64.iso" ;;
     "kubuntu" )
@@ -44,25 +44,25 @@ getURL() {
     "macos" | "osx" )
       name="macOS"
       error "To install $name use: https://github.com/dockur/macos" && return 1 ;;
-    "mint" | "linuxmint" )
+    "mint" | "linuxmint" | "linux-mint" )
       name="Linux Mint"
       url="https://mirrors.layeronline.com/linuxmint/stable/22.1/linuxmint-22.1-cinnamon-64bit.iso" ;;
     "manjaro" )
       name="Manjaro"
       url="https://download.manjaro.org/kde/24.2.1/manjaro-kde-24.2.1-241216-linux612.iso" ;;
-    "mx" )
+    "mx" | "mxlinux" | "mx-linux" )
       name="MX Linux"
       url="https://mirror.umd.edu/mxlinux-iso/MX/Final/Xfce/MX-23.5_x64.iso" ;;
     "nixos" )
       name="NixOS"
       url="https://channels.nixos.org/nixos-24.11/latest-nixos-gnome-x86_64-linux.iso" ;;
-    "opensuse" | "suse" )
+    "opensuse" | "open-suse" | "suse" )
       name="OpenSUSE"
       url="https://download.opensuse.org/distribution/leap/15.0/live/openSUSE-Leap-15.0-GNOME-Live-x86_64-Current.iso" ;;
-    "oracle" )
+    "oracle" | "oraclelinux" | "oracle-linux" )
       name="Oracle Linux"
       url="https://yum.oracle.com/ISOS/OracleLinux/OL9/u5/x86_64/OracleLinux-R9-U5-x86_64-boot.iso" ;;
-    "rocky" )
+    "rocky" | "rockylinux" | "rocky-linux" )
       name="Rocky Linux"
       url="https://dl.rockylinux.org/pub/rocky/9/live/x86_64/Rocky-9-Workstation-x86_64-latest.iso" ;;
     "slack" | "slackware" )
