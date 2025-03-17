45965c10d4014b49fd5f6e789cb9be37d6c4350d
Kroese <kroese@users.noreply.github.com>
2025-03-17 15:00:20 +0100

    Update reset.sh

--- src/reset.sh
+++ src/reset.sh
@@ -306,9 +306,10 @@ fi
 # Set password
 echo "$user:{PLAIN}${PASS:-}" > /etc/nginx/.htpasswd
 
-ipv6=""
+# Check if IPv6 is supported
+ipv6=$(ifconfig -a | grep inet6)
 
-if [ -n "$ipv6" ]; then
+if [ -f /proc/net/if_inet6 ] && [ -n "$ipv6" ]; then
 
   sed -i "s/listen 80/listen [::]:80 ipv6only=off/g" /etc/nginx/sites-enabled/web.conf
   sed -i "s/listen 8006 default_server/listen [::]:8006 default_server ipv6only=off/g" /etc/nginx/sites-enabled/web.conf
