f2f0f4ae41fbdf254394b4ffda1db7e7a43deae2
Kroese <kroese@users.noreply.github.com>
2025-03-17 15:06:15 +0100

    Update reset.sh

--- src/reset.sh
+++ src/reset.sh
@@ -311,13 +311,13 @@ ipv6=$(ifconfig -a | grep inet6)
 
 if [ -f /proc/net/if_inet6 ] && [ -n "$ipv6" ]; then
 
-  sed -i "s/listen 80/listen [::]:80 ipv6only=off/g" /etc/nginx/sites-enabled/web.conf
-  sed -i "s/listen 8006 default_server/listen [::]:8006 default_server ipv6only=off/g" /etc/nginx/sites-enabled/web.conf
+  sed -i "s/listen 80;/listen [::]:80 ipv6only=off;/g" /etc/nginx/sites-enabled/web.conf
+  sed -i "s/listen 8006 default_server;/listen [::]:8006 default_server ipv6only=off;/g" /etc/nginx/sites-enabled/web.conf
 
 else
 
-  sed -i "s/listen [::]:80 ipv6only=off/listen 80/g" /etc/nginx/sites-enabled/web.conf
-  sed -i "s/listen [::]:8006 default_server ipv6only=off/listen 8006 default_server/g" /etc/nginx/sites-enabled/web.conf
+  sed -i "s/listen [::]:80 ipv6only=off;/listen 80;/g" /etc/nginx/sites-enabled/web.conf
+  sed -i "s/listen [::]:8006 default_server ipv6only=off;/listen 8006 default_server;/g" /etc/nginx/sites-enabled/web.conf
 
 fi
 
