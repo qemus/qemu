088d32ba074adfc551f81488f3fd4c42717fbc48
Kroese <kroese@users.noreply.github.com>
2025-09-20 13:42:35 +0200

    Implement retry mechanism for failed downloads
    Added retry logic for downloadFile function in install.sh.

--- src/install.sh
+++ src/install.sh
@@ -342,7 +342,19 @@ find "$STORAGE" -maxdepth 1 -type f \( -iname 'data.*' -or -iname 'qemu.*' \) -d
 
 base=$(getBase "$BOOT")
 
-downloadFile "$BOOT" "$base" "$name"
+rm -f "$STORAGE/$base"
+
+if ! downloadFile "$BOOT" "$base" "$name"; then
+  info "Retrying failed download in 5 seconds..."
+  sleep 5
+  if ! downloadFile "$BOOT" "$base" "$name"; then
+    info "Retrying failed download in 5 seconds..."
+    sleep 5  
+    if ! downloadFile "$BOOT" "$base" "$name"; then
+      rm -f "$STORAGE/$base" && exit 60
+    fi
+  fi
+fi
 
 case "${base,,}" in
   *".gz" | *".gzip" | *".xz" | *".7z" | *".zip" | *".rar" | *".lzma" | *".bz" | *".bz2" )
