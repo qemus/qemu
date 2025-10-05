f57ca33a709f0c1332a6cfb1a15b694302ba251b
Kroese <kroese@users.noreply.github.com>
2025-10-05 17:00:55 +0200

    fix: Exclude 'tap' from bus check

--- src/network.sh
+++ src/network.sh
@@ -544,7 +544,7 @@ getInfo() {
   nic=$(grep -m 1 -i 'driver:' <<< "$result" | awk '{print $(2)}')
   bus=$(grep -m 1 -i 'bus-info:' <<< "$result" | awk '{print $(2)}')
 
-  if [[ "${bus,,}" != "" && "${bus,,}" != "n/a" ]]; then
+  if [[ "${bus,,}" != "" && "${bus,,}" != "n/a" && "${bus,,}" != "tap" ]]; then
     [[ "$DEBUG" == [Yy1]* ]] && info "Detected BUS: $bus"
     error "This container does not support host mode networking!"
     exit 29
