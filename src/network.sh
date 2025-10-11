b1236dc838ffe1f0fed20c7eef3c714da73df133
Kroese <kroese@users.noreply.github.com>
2025-10-12 00:57:56 +0200

    Update network.sh

--- src/network.sh
+++ src/network.sh
@@ -813,7 +813,7 @@ else
       if ! configureSlirp; then
         error "Failed to configure user-mode networking!"
         exit 24
-      fi
+      fi ;;
 
     *)
       error "Unrecognized NETWORK value: \"$NETWORK\"" && exit 24 ;;
