ab26195b7d36e33e9fa5fb5d7c01f1c92a939c08
Kroese <kroese@users.noreply.github.com>
2025-10-17 17:21:20 +0200

    fix: Use temporary file when extracting ROM

--- src/boot.sh
+++ src/boot.sh
@@ -74,16 +74,17 @@ case "${BOOT_MODE,,}" in
     if [ ! -s "$DEST.rom" ] || [ ! -f "$DEST.rom" ]; then
       [ ! -s "$OVMF/$ROM" ] || [ ! -f "$OVMF/$ROM" ] && error "UEFI boot file ($OVMF/$ROM) not found!" && exit 44
       if [[ "${LOGO:-}" == [Nn]* ]]; then
-        cp "$OVMF/$ROM" "$DEST.rom"
+        cp "$OVMF/$ROM" "$DEST.tmp"
       else
         /run/utk.bin "$OVMF/$ROM" replace_ffs LogoDXE "/var/www/img/${PROCESS,,}.ffs" save "$DEST.tmp"
-        mv "$DEST.tmp" "$DEST.rom"
       fi
+      mv "$DEST.tmp" "$DEST.rom"
     fi
 
     if [ ! -s "$DEST.vars" ] || [ ! -f "$DEST.vars" ]; then
       [ ! -s "$OVMF/$VARS" ] || [ ! -f "$OVMF/$VARS" ]&& error "UEFI vars file ($OVMF/$VARS) not found!" && exit 45
-      cp "$OVMF/$VARS" "$DEST.vars"
+      cp "$OVMF/$VARS" "$DEST.tmp"
+      mv "$DEST.tmp" "$DEST.vars"
     fi
 
     if [[ "${BOOT_MODE,,}" == "secure" || "${BOOT_MODE,,}" == "windows_secure" ]]; then
