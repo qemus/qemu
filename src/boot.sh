ba2d171304b637032ab2d210f59d3535f92b0fd8
Kroese <kroese@users.noreply.github.com>
2025-10-17 16:56:06 +0200

    fix: Use temporary file when extracting ROM (#929)

--- src/boot.sh
+++ src/boot.sh
@@ -73,9 +73,11 @@ case "${BOOT_MODE,,}" in
 
     if [ ! -s "$DEST.rom" ] || [ ! -f "$DEST.rom" ]; then
       [ ! -s "$OVMF/$ROM" ] || [ ! -f "$OVMF/$ROM" ] && error "UEFI boot file ($OVMF/$ROM) not found!" && exit 44
-      cp "$OVMF/$ROM" "$DEST.rom"
-      if [[ "${LOGO:-}" != [Nn]* ]]; then
-        /run/utk.bin "$DEST.rom" replace_ffs LogoDXE "/var/www/img/${PROCESS,,}.ffs" save "$DEST.rom"
+      if [[ "${LOGO:-}" == [Nn]* ]]; then
+        cp "$OVMF/$ROM" "$DEST.rom"
+      else
+        /run/utk.bin "$OVMF/$ROM" replace_ffs LogoDXE "/var/www/img/${PROCESS,,}.ffs" save "$DEST.tmp"
+        mv "$DEST.tmp" "$DEST.rom"
       fi
     fi
 
