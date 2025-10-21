726394d4ad14952c85c07c446507e59bb0420981
Kroese <kroese@users.noreply.github.com>
2025-10-21 22:15:57 +0200

    Update network.sh

--- src/network.sh
+++ src/network.sh
@@ -390,7 +390,12 @@ configurePasst() {
 
   PASST_OPTS+=" -H $VM_NET_HOST"
   PASST_OPTS+=" -M $GATEWAY_MAC"
-  PASST_OPTS+=" --runas $UID:$GID"
+
+  local uid gid
+  uid=$(id -u)
+  gid=$(id -g)
+  PASST_OPTS+=" --runas $uid:$gid"
+
   PASST_OPTS+=" -P /var/run/passt.pid"
   PASST_OPTS+=" -l $log"
   PASST_OPTS+=" -q"
