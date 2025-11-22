6a91935b6ae9ab4435a70773f13f47e557e4e205
synacktra.work@gmail.com <synacktra.work@gmail.com>
2025-11-22 18:16:54 +0530

    feat: add graceful shutdown support

--- src/entry.sh
+++ src/entry.sh
@@ -17,6 +17,7 @@ cd /run
 . network.sh    # Initialize network
 . boot.sh       # Configure boot
 . proc.sh       # Initialize processor
+. power.sh      # Configure shutdown
 . memory.sh     # Check available memory
 . config.sh     # Configure arguments
 . finish.sh     # Finish initialization
@@ -24,6 +25,20 @@ cd /run
 trap - ERR
 
 version=$(qemu-system-x86_64 --version | head -n 1 | cut -d '(' -f 1 | awk '{ print $NF }')
-info "Booting image${BOOT_DESC} using QEMU v$version..."
+info "Booting ${APP}${BOOT_DESC} using QEMU v$version..."
 
-exec qemu-system-x86_64 ${ARGS:+ $ARGS}
+{ qemu-system-x86_64 ${ARGS:+ $ARGS} >"$QEMU_OUT" 2>"$QEMU_LOG"; rc=$?; } || :
+(( rc != 0 )) && error "$(<"$QEMU_LOG")" && exit 15
+
+terminal
+( sleep 30; boot ) &
+tail -fn +0 "$QEMU_LOG" --pid=$$ 2>/dev/null &
+cat "$QEMU_TERM" 2> /dev/null | tee "$QEMU_PTY" | \
+sed -u -e 's/\x1B\[[=0-9;]*[a-z]//gi' \
+-e 's/\x1B\x63//g' -e 's/\x1B\[[=?]7l//g' \
+-e '/^$/d' -e 's/\x44\x53\x73//g' \
+-e 's/failed to load Boot/skipped Boot/g' \
+-e 's/0): Not Found/0)/g' & wait $! || :
+
+sleep 1 & wait $!
+[ ! -f "$QEMU_END" ] && finish 0
