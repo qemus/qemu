6f309561e05e729ce8cdc1e740b82af77b216be2
Kroese <kroese@users.noreply.github.com>
2026-05-06 19:26:32 +0200

    Add PID file by default
    Updated the MONITOR variable to include a pidfile option.

--- src/config.sh
+++ src/config.sh
@@ -6,8 +6,8 @@ set -Eeuo pipefail
 : "${VMPORT:="off"}"
 : "${SERIAL:="mon:stdio"}"
 : "${USB:="qemu-xhci,id=xhci,p2=7,p3=7"}"
-: "${MONITOR:="telnet:localhost:$MON_PORT,server,nowait,nodelay"}"
 : "${SMP:="$CPU_CORES,sockets=1,dies=1,cores=$CPU_CORES,threads=1"}"
+: "${MONITOR:="telnet:localhost:$MON_PORT,server,nowait,nodelay -pidfile $QEMU_PID"}"
 
 msg="Configuring QEMU..."
 html "$msg"
@@ -18,7 +18,7 @@ DEF_OPTS="-nodefaults"
 SERIAL_OPTS="-serial $SERIAL"
 CPU_OPTS="-cpu $CPU_FLAGS -smp $SMP"
 RAM_OPTS=$(echo "-m ${RAM_SIZE^^}" | sed 's/MB/M/g;s/GB/G/g;s/TB/T/g')
-MON_OPTS="-monitor $MONITOR -name $PROCESS,process=$PROCESS,debug-threads=on -pidfile $QEMU_PID"
+MON_OPTS="-monitor $MONITOR -name $PROCESS,process=$PROCESS,debug-threads=on"
 MAC_OPTS="-machine type=${MACHINE},smm=${SECURE},graphics=off,vmport=${VMPORT},dump-guest-core=off,hpet=${HPET}${KVM_OPTS}"
 
 [ -n "$UUID" ] && MAC_OPTS+=" -uuid $UUID"
