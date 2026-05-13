08c5d9092e277431ab0f668642e8986b3dbd7ead
Kroese <kroese@users.noreply.github.com>
2026-05-13 08:46:03 +0200

    fix: Use different driver for .vhdx files

--- src/install.sh
+++ src/install.sh
@@ -478,7 +478,7 @@ target_fmt="${DISK_FMT:-}"
 case "${base,,}" in
   *".vdi" ) source_fmt="vdi" ;;
   *".vhd" ) source_fmt="vpc" ;;
-  *".vhdx" ) source_fmt="vpc" ;;
+  *".vhdx" ) source_fmt="vhdx" ;;
   *".vmdk" ) source_fmt="vmdk" ;;
   * ) error "Unknown file extension, type \".${base/*./}\" is not recognized!" && exit 33 ;;
 esac
