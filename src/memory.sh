f2c915bfeaa2c2d0f27a0d7dde312081ee8d0b29
Kroese <kroese@users.noreply.github.com>
2026-05-15 01:27:25 +0200

    Simplify RAM logic

--- src/memory.sh
+++ src/memory.sh
@@ -2,16 +2,18 @@
 set -Eeuo pipefail
 
 msg="Checking memory..."
+
 html "$msg"
 [[ "$DEBUG" == [Yy1]* ]] && echo "$msg"
 
 RAM_AVAIL=$(free -b | grep -m 1 Mem: | awk '{print $7}')
+AVAIL_MEM=$(formatBytes "$RAM_AVAIL")
 
 if [[ "$RAM_CHECK" != [Nn]* && "${RAM_SIZE,,}" != "max" && "${RAM_SIZE,,}" != "half" ]]; then
 
-  AVAIL_MEM=$(formatBytes "$RAM_AVAIL")
+  wanted=$(numfmt --from=iec "$RAM_SIZE")
 
-  if (( (RAM_WANTED + RAM_SPARE) > RAM_AVAIL )); then
+  if (( (wanted + RAM_SPARE) > RAM_AVAIL )); then
     msg="Your configured RAM_SIZE of ${RAM_SIZE/G/ GB} is too high for the $AVAIL_MEM of memory available,"
     if [[ "${FS,,}" == "zfs" ]]; then
       info "$msg but since ZFS is active this will be ignored."
@@ -20,7 +22,7 @@ if [[ "$RAM_CHECK" != [Nn]* && "${RAM_SIZE,,}" != "max" && "${RAM_SIZE,,}" != "h
       warn "$msg it will automatically be adjusted to a lower amount."
     fi
   else
-    if (( (RAM_WANTED + (RAM_SPARE * 3)) > RAM_AVAIL )); then
+    if (( (wanted + (RAM_SPARE * 3)) > RAM_AVAIL )); then
       msg="your configured RAM_SIZE of ${RAM_SIZE/G/ GB} is very close to the $AVAIL_MEM of memory available,"
       if [[ "${FS,,}" == "zfs" ]]; then
         info "$msg but since ZFS is active this will be ignored."
@@ -34,20 +36,54 @@ fi
 
 if [[ "${RAM_SIZE,,}" == "half" ]]; then
 
-  RAM_WANTED=$(( RAM_AVAIL / 2 ))
-  RAM_WANTED=$(( RAM_WANTED / 1073741825 ))
-
-  if (( "$RAM_WANTED" < 1 )); then
-    RAM_WANTED=$(( RAM_AVAIL / 2 ))
-    RAM_WANTED=$(( RAM_WANTED / 1048577 ))
-    RAM_SIZE="${RAM_WANTED}M"
+  if (( (RAM_AVAIL / 2) > RAM_SPARE )); then
+    wanted=$(( (RAM_AVAIL / 2) / 1048577 ))
+    RAM_SIZE="${wanted}M"
   else
-    RAM_SIZE="${RAM_WANTED}G"
+    RAM_SIZE="max"
   fi
 
 fi
 
 if [[ "${RAM_SIZE,,}" == "max" ]]; then
+
+  if (( RAM_AVAIL > RAM_SPARE )); then
+
+    wanted=(( (RAM_AVAIL - (RAM_SPARE * 3)) / 1048577 ))
+
+    if (( wanted < 1024 )); then
+
+    fi
+
+
+
+
+
+  else
+
+
+
+
+
+
+
+
+
+  
+
+    
+    
+    if (( i < 1 )); then
+
+      wanted=(( (RAM_AVAIL - RAM_SPARE) / 1048577 ))
+      (( wanted < 1 )) && wanted=(( RAM_AVAIL / 1048577 ))
+      RAM_SIZE="${m}M"
+
+    fi
+
+  done
+
+fi
 
   RAM_WANTED=$(( RAM_AVAIL - (RAM_SPARE * 3) ))
   RAM_WANTED=$(( RAM_WANTED / 1073741825 ))
@@ -83,6 +119,18 @@ if [[ "${RAM_SIZE,,}" == "max" ]]; then
     fi
   else
     RAM_SIZE="${RAM_WANTED}G"
+  fi
+
+fi
+
+if [[ "$RAM_CHECK" != [Nn]* ]]; then
+
+  wanted=$(numfmt --from=iec "$RAM_SIZE")
+
+  if [ "$wanted" -lt "$RAM_MINIMUM" ]; then
+    wanted=$(( wanted / 1048577 ))
+    error "Not enough memory available, only $wanted MB left!"
+    exit 16
   fi
 
 fi
