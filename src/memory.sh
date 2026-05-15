e8e02f02c9123d513360fe2d80d81764e9a4eccc
Kroese <kroese@users.noreply.github.com>
2026-05-15 13:10:42 +0200

    Add memory messages

--- src/memory.sh
+++ src/memory.sh
@@ -14,7 +14,7 @@ if [[ "$RAM_CHECK" != [Nn]* && "${RAM_SIZE,,}" != "max" && "${RAM_SIZE,,}" != "h
   wanted=$(numfmt --from=iec "$RAM_SIZE")
 
   if (( (wanted + RAM_SPARE) > RAM_AVAIL )); then
-    msg="Your configured RAM_SIZE of ${RAM_SIZE/G/ GB} is too high for the $AVAIL_MEM of memory available,"
+    msg="Your configured RAM_SIZE of ${RAM_SIZE/G/ GB} is too high for the $AVAIL_MEM of free memory available,"
     if [[ "${FS,,}" == "zfs" ]]; then
       info "$msg but since ZFS is active this will be ignored."
     else
@@ -23,7 +23,7 @@ if [[ "$RAM_CHECK" != [Nn]* && "${RAM_SIZE,,}" != "max" && "${RAM_SIZE,,}" != "h
     fi
   else
     if (( (wanted + (RAM_SPARE * 3)) > RAM_AVAIL )); then
-      msg="your configured RAM_SIZE of ${RAM_SIZE/G/ GB} is very close to the $AVAIL_MEM of memory available,"
+      msg="your configured RAM_SIZE of ${RAM_SIZE/G/ GB} is very close to the $AVAIL_MEM of free memory available,"
       if [[ "${FS,,}" == "zfs" ]]; then
         info "$msg but since ZFS is active this will be ignored."
       else
@@ -39,6 +39,7 @@ if [[ "${RAM_SIZE,,}" == "half" ]]; then
   if (( (RAM_AVAIL / 2) > RAM_SPARE )); then
     wanted=$(( (RAM_AVAIL / 2) / 1048577 ))
     RAM_SIZE="${wanted}M"
+    echo "Allocated a total of $wanted MB of RAM for the virtual machine, which is half of the $AVAIL_MEM of free memory available."
   else
     RAM_SIZE="max"
   fi
@@ -63,6 +64,8 @@ if [[ "${RAM_SIZE,,}" == "max" ]]; then
 
   wanted=$(( wanted / 1048577 ))
   RAM_SIZE="${wanted}M"
+
+  echo "Allocated a total of $wanted MB of RAM for the virtual machine, as there was $AVAIL_MEM of free memory available."
 
 fi
 
