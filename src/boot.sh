daa14fe16d91e518f23bba02addc5dc612d051e2
Kroese <kroese@users.noreply.github.com>
2026-05-16 19:03:08 +0200

    fix: Do not mask swtmp exit code (#1043)
    Refactor TPM emulator startup command to capture exit code.

--- src/boot.sh
+++ src/boot.sh
@@ -147,8 +147,10 @@ rm -f /var/run/tpm.pid
 
 if [[ "$TPM" == [Yy1]* ]]; then
 
-  if ! swtpm socket -t -d --tpmstate "backend-uri=file://$STORAGE/${BOOT_MODE,,}.tpm" --ctrl type=unixio,path=/run/swtpm-sock --pid file=/var/run/tpm.pid --tpm2; then
-    error "Failed to start TPM emulator, reason: $?"
+  { swtpm socket -t -d --tpmstate "backend-uri=file://$STORAGE/${BOOT_MODE,,}.tpm" --ctrl type=unixio,path=/run/swtpm-sock --pid file=/var/run/tpm.pid --tpm2; rc=$?; } || :
+
+  if (( rc != 0 )); then
+    error "Failed to start TPM emulator, reason: $rc"
   else
 
     for (( i = 1; i < 20; i++ )); do
