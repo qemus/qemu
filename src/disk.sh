77f4125c8124fad4052e65d671b525d452bd923a
Kroese <kroese@users.noreply.github.com>
2026-05-30 20:51:09 +0200

    feat: Added finishDisks function (#1077)

--- src/disk.sh
+++ src/disk.sh
@@ -461,6 +461,20 @@ addMedia () {
   return 0
 }
 
+finishDisks () {
+
+  case "${DISK_TYPE,,}" in
+    "blk" | "scsi" | "virtio-blk" | "virtio-scsi" )
+      DISK_OPTS+=" -object iothread,id=io2" ;;
+  esac
+
+  if [[ "$DISK_DISABLE" != [Yy1]* ]]; then
+    html "Initialized disks successfully..."
+  fi
+
+  return 0
+}
+
 addDisk () {
 
   local DISK_BASE="$1"
@@ -628,14 +642,16 @@ addDevice () {
   return 0
 }
 
-msg="Initializing disks..."
-html "$msg"
-[[ "$DEBUG" == [Yy1]* ]] && echo "$msg"
-
 [ -z "${DISK_OPTS:-}" ] && DISK_OPTS=""
 [ -z "${DISK_TYPE:-}" ] && DISK_TYPE="scsi"
 [ -z "${DISK_NAME:-}" ] && DISK_NAME="data"
 [ -z "${DISK_DISABLE:-}" ] && DISK_DISABLE=""
+
+if [[ "$DISK_DISABLE" != [Yy1]* ]]; then
+  msg="Initializing disks..."
+  html "$msg"
+  [[ "$DEBUG" == [Yy1]* ]] && echo "$msg"
+fi
 
 case "${DISK_TYPE,,}" in
   "ide" | "sata" | "nvme" | "usb" | "scsi" | "blk" | "auto" | "none" ) ;;
@@ -732,11 +748,7 @@ else
 fi
 
 if [[ "$DISK_DISABLE" == [Yy1]* ]]; then
-  case "${DISK_TYPE,,}" in
-    "blk" | "scsi" | "virtio-blk" | "virtio-scsi" )
-      DISK_OPTS+=" -object iothread,id=io2" ;;
-  esac
-  return 0
+  finishDisks && return 0
 fi
 
 : "${DISK2_SIZE:=""}"
@@ -803,10 +815,6 @@ else
   addDisk "$DISK6_FILE" "$DISK_TYPE" "disk6" "$DISK6_SIZE" "8" "0xf" "$DISK_FMT" "$DISK_IO" "$DISK_CACHE" || exit $?
 fi
 
-case "${DISK_TYPE,,}" in
-  "blk" | "scsi" | "virtio-blk" | "virtio-scsi" )
-    DISK_OPTS+=" -object iothread,id=io2" ;;
-esac
+finishDisks
 
-html "Initialized disks successfully..."
 return 0
