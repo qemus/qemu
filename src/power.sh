e18fb9d7d5e23ca5cb3b4eb13e9aa35789630c3a
Kroese <kroese@users.noreply.github.com>
2026-06-01 22:52:05 +0200

    Update power.sh

--- src/power.sh
+++ src/power.sh
@@ -114,8 +114,8 @@ _graceful_shutdown() {
     fi
 
     # Send ACPI shutdown signal
-    if [ -S "$QEMU_DIR/qmp.sock" ]; then
-      nc -q 1 -w 1 -U "$QEMU_DIR/qmp.sock" > /dev/null <<<'system_powerdown' || :
+    if [ -S "$QEMU_DIR/monitor.sock" ]; then
+      nc -q 1 -w 1 -U "$QEMU_DIR/monitor.sock" > /dev/null <<<'system_powerdown' || :
     fi
 
     wait $slp
