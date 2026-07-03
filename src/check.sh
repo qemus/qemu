64a13e91c351c7ae24f301e4c906bd8d857f90a9
Kroese <kroese@users.noreply.github.com>
2026-07-03 15:14:45 +0200

    fix: Update networking check (#1126)

--- src/check.sh
+++ src/check.sh
@@ -5,7 +5,7 @@ set -Eeuo pipefail
 
 [ -f "/run/shm/qemu.end" ] && echo "QEMU is shutting down..." && exit 1
 [ ! -s "/run/shm/qemu.pid" ] && echo "QEMU is not running yet..." && exit 0
-disabled "$NETWORK" && echo "Networking is disabled." && exit 0
+[[ "$NETWORK" == [Nn]* ]] && echo "Networking is disabled." && exit 0
 
 file="/run/shm/qemu.url"
 [ ! -s "$file" ] && echo "The container has not enabled networking yet..." && exit 1
