4877d6c1fc3f7e666a87e0d5349b6d0b93b5a1b1
Kroese <kroese@users.noreply.github.com>
2026-07-03 15:42:19 +0200

    Update disk.sh

--- src/disk.sh
+++ src/disk.sh
@@ -11,6 +11,7 @@ set -Eeuo pipefail
 : "${DISK_DISCARD:="on"}"         # Controls whether unmap (TRIM) commands are passed to the host.
 : "${DISK_ROTATION:="1"}"         # Rotation rate, set to 1 for SSD storage and increase for HDD
 
+# Sanitize all variables
 DISK_IO=$(strip "$DISK_IO")
 DISK_FMT=$(strip "$DISK_FMT")
 DISK_TYPE=$(strip "$DISK_TYPE")
