5364b9f0834f59b22791c8c7ccfe83950df14728
Kroese <kroese@users.noreply.github.com>
2026-07-05 03:10:38 +0200

    Update network.sh

--- src/network.sh
+++ src/network.sh
@@ -31,7 +31,6 @@ set -Eeuo pipefail
 : "${DNSMASQ_DEBUG:=""}"
 : "${DNSMASQ:="/usr/sbin/dnsmasq"}"
 : "${DNSMASQ_PID:="/var/run/dnsmasq.pid"}"
-: "${DNSMASQ_CONF_DIR:="/etc/dnsmasq.d"}"
 
 # Sanitize variables
 MAC=$(strip "$MAC")
@@ -692,8 +691,8 @@ closeNetwork() {
 
   disabled "$NETWORK" && return 0
 
-  exec 30<&- || true
-  exec 40<&- || true
+  exec 30>&- 2>/dev/null || true
+  exec 40>&- 2>/dev/null || true
 
   closeBridge
 
