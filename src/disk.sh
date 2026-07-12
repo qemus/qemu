b4054d295e4c3d5038ba5bcf11e001847fd06431
Kroese <kroese@users.noreply.github.com>
2026-07-12 22:38:33 +0200

    feat: Add disk validation function

--- src/disk.sh
+++ src/disk.sh
@@ -89,6 +89,18 @@ supportsDirect() {
   return 0
 }
 
+validDiskType() {
+
+  case "${1,,}" in
+    "ide" | "sata" | "nvme" | "usb" | "scsi" | "blk" | \
+    "virtio-blk" | "virtio-scsi" | "auto" | "none" )
+      return 0
+      ;;
+  esac
+
+  return 1
+}
+
 allocateRaw() {
 
   local DISK_FILE="$1"
@@ -762,10 +774,10 @@ if [[ ! "$DISK_ROTATION" =~ ^[0-9]+$ ]]; then
   DISK_ROTATION="1"
 fi
 
-case "${DISK_TYPE,,}" in
-  "ide" | "sata" | "nvme" | "usb" | "scsi" | "blk" | "virtio-blk" | "virtio-scsi" | "auto" | "none" ) ;;
-  * ) error "Invalid DISK_TYPE specified, value \"$DISK_TYPE\" is not recognized!" && exit 80 ;;
-esac
+if ! validDiskType "$DISK_TYPE"; then
+  error "Invalid DISK_TYPE specified, value \"$DISK_TYPE\" is not recognized!"
+  exit 80
+fi
 
 if [[ "$DISK_FLAGS" =~ [[:space:]] ]]; then
   error "Invalid DISK_FLAGS value '$DISK_FLAGS', spaces are not allowed."
@@ -792,10 +804,10 @@ if [ -z "${MEDIA_TYPE:-}" ]; then
   fi
 fi
 
-case "${MEDIA_TYPE,,}" in
-  "ide" | "sata" | "nvme" | "usb" | "scsi" | "blk" | "virtio-blk" | "virtio-scsi" | "auto" | "none" ) ;;
-  * ) error "Invalid MEDIA_TYPE specified, value \"$MEDIA_TYPE\" is not recognized!" && exit 80 ;;
-esac
+if ! validDiskType "$MEDIA_TYPE"; then
+  error "Invalid MEDIA_TYPE specified, value \"$MEDIA_TYPE\" is not recognized!"
+  exit 80
+fi
 
 if [ -s "$BOOT" ]; then
   case "${BOOT,,}" in
