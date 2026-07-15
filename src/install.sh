e9cdbdb672b890cbc07dc4c3129e9b139b524581
Kroese <kroese@users.noreply.github.com>
2026-07-15 17:55:53 +0200

    Update install.sh

--- src/install.sh
+++ src/install.sh
@@ -566,7 +566,7 @@ case "${base,,}" in
 
   *".iso" | *".img" | *".raw" | *".qcow2" )
 
-    ! setOwner "$STORAGE/$base" && error "Failed to set the owner for \"$STORAGE/$base\" !"
+    ! setOwner "$STORAGE/$base" && warn "failed to set the owner for \"$STORAGE/$base\" !"
     detectType "$STORAGE/$base" && return 0
     error "Cannot read file \"${base}\"" && exit 63 ;;
 
@@ -591,7 +591,7 @@ dst="$STORAGE/${base%.*}.$target_ext"
 
 base=$(basename "$dst")
 
-! setOwner "$STORAGE/$base" && error "Failed to set the owner for \"$STORAGE/$base\" !"
+! setOwner "$STORAGE/$base" && warn "failed to set the owner for \"$STORAGE/$base\" !"
 detectType "$STORAGE/$base" && return 0
 error "Cannot convert file \"${base}\"" && exit 36
 
