1e9a4027ec54f7ab003761f98ba9b21a119f6ed0
Kroese <kroese@users.noreply.github.com>
2026-07-17 20:22:22 +0200

    feat: Add hasFlag helper for CPU flags

--- src/utils.sh
+++ src/utils.sh
@@ -7,6 +7,13 @@ info () { printf "%b%s%b" "\E[1;34m❯ \E[1;36m" "${1:-}" "\E[0m\n"; }
 error () { printf "%b%s%b" "\E[1;31m❯ " "ERROR: ${1:-}" "\E[0m\n" >&2; }
 warn () { printf "%b%s%b" "\E[1;31m❯ " "Warning: ${1:-}" "\E[0m\n" >&2; }
 
+hasFlag() {
+
+  # Match a whitespace-delimited token in /proc/cpuinfo
+  grep -m1 '^flags[[:space:]]*:' /proc/cpuinfo | grep -Fqw -- "$1"
+
+}
+
 interactive() {
 
   [ -t 1 ] &&
