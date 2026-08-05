6402638b7eb2260975315233f747474bfaa6b72c
Kroese <kroese@users.noreply.github.com>
2026-08-05 19:29:26 +0200

    fid: Improve directory check logic

--- src/install.sh
+++ src/install.sh
@@ -603,16 +603,14 @@ findFile() {
 
   # A directory at a file bind path usually means the host source did not exist
   # when the container runtime created the mount point.
-  if [ -d "$dir" ]; then
-
-    hasData && return 1
-
+  if [ -d "$dir" ] && ! hasDisk; then
     error "The bind $dir maps to a file that does not exist!"
     exit 37
   fi
 
   file=$(find / -maxdepth 1 -type f -iname "$fname" -print -quit)
   [ ! -s "$file" ] && file=$(find "$STORAGE" -maxdepth 1 -type f -iname "$fname" -print -quit)
+  [ ! -s "$file" ] && return 1
 
   detectType "$file" && return 0
 
