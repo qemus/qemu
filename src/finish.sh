e879a67db5d701742dea9c9cd74a5b603b79b4bc
Kroese <kroese@users.noreply.github.com>
2026-08-06 23:18:20 +0200

    Update finish.sh

--- src/finish.sh
+++ src/finish.sh
@@ -2,20 +2,28 @@
 set -Eeuo pipefail
 
 if [[ "${DISPLAY,,}" == "web" ]]; then
+
   [ ! -f "$INFO" ] && error "File $INFO not found?!"
   rm -f "$INFO"
+
   [ ! -f "$PAGE" ] && error "File $PAGE not found?!"
   rm -f "$PAGE"
+
 else
+
   if [[ "${DISPLAY,,}" == "vnc" ]]; then
     html "You can now connect to VNC on port $VNC_PORT." "0"
   else
     html "The virtual machine was booted successfully." "0"
   fi
+
 fi
 
 if enabled "$DEBUG"; then
   printf "QEMU arguments:\n\n    %s\n\n" "${ARGS// -/$'\n    -'}"
 fi
+
+# Must always remain the very last command
+enableTrap
 
 return 0
