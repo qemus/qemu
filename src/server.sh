8d4fecc1b2f56daf2a2c96334715aed732c112a0
Kroese <kroese@users.noreply.github.com>
2026-08-09 17:35:22 +0200

    Update server.sh

--- src/server.sh
+++ src/server.sh
@@ -15,6 +15,7 @@ AUX_PID="$QEMU_DIR/audio-websocketd.pid"
 WSS_SOCKET="$QEMU_DIR/vnc-ws.sock"
 AUX_SOCKET="$QEMU_DIR/audio-ws.sock"
 WSD_SOCKET="$QEMU_DIR/status-ws.sock"
+WSD_COMMAND="$QEMU_DIR/status.cmd"
 
 WSD_LOG="/var/log/websocketd.log"
 AUX_LOG="/var/log/audio-socket.log"
@@ -34,7 +35,7 @@ prepareWebFiles() {
   cp -r /var/www/* "$QEMU_DIR" || return 1
   rm -f -- \
     "$WSD_PID" "$AUX_PID" "$WEB_PID" \
-    "$WSD_SOCKET" "$AUX_SOCKET" \
+    "$WSD_SOCKET" "$AUX_SOCKET" "$WSD_COMMAND" \
     "$WSD_LOG" "$AUX_LOG" || return 1
 
   return 0
